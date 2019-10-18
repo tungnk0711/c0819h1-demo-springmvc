a9e357e859eb9a9602dc98a2b28a945249137120
nguyenkhanhtung <tungnk0711@gmail.com>
2019-10-18 14:27:22 +0700

    update controller

--- src/main/java/com/codegym/controller/ProductController.java
+++ src/main/java/com/codegym/controller/ProductController.java
@@ -4,22 +4,23 @@ import com.codegym.model.Product;
 import com.codegym.service.ProductService;
 import com.codegym.service.ProductServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.HttpRequestHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
+@RequestMapping("/product")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/products")
+    @RequestMapping(value = "/list*", method = RequestMethod.GET)
     public ModelAndView getProducts(){
         List<Product> productList  = productService.findAllHaveBusiness();
         ModelAndView modelAndView = new ModelAndView("/product/list");
@@ -32,21 +33,36 @@ public class ProductController {
     public ModelAndView createForm(){
         ModelAndView modelAndView = new ModelAndView("/product/create");
         modelAndView.addObject("message","Add Product");
+        modelAndView.addObject("product", new Product());
         return modelAndView;
     }
 
-    @PostMapping("save-product")
-    public ModelAndView saveProduct(HttpServletRequest httpServletRequest){
+    @PostMapping("/save-product")
+    public ModelAndView saveProduct(@ModelAttribute Product product){
 
-        int id = Integer.parseInt(httpServletRequest.getParameter("id"));
-        String name = httpServletRequest.getParameter("name");
-        Double price = Double.parseDouble(httpServletRequest.getParameter("price"));
-
-        Product product = new Product(id, name, price);
         productService.addHaveBusiness(product);
 
 
         ModelAndView modelAndView = new ModelAndView("/product/create");
+        return modelAndView;
+    }
+
+    @GetMapping("/search-by-name")
+    public ModelAndView searchByName(@RequestParam("name") String name){
+
+        List<Product> productList  = productService.findAllHaveBusiness();
+        ModelAndView modelAndView = new ModelAndView("/product/list");
+        modelAndView.addObject("products", productList);
+        modelAndView.addObject("message","Danh sach san pham");
+        return modelAndView;
+
+    }
+
+    @GetMapping("/edit-product/{id}/{address}")
+    public ModelAndView editForm(@PathVariable("id") Long id, @PathVariable("address") String address){
+
+
+        ModelAndView modelAndView = new ModelAndView("/product/edit");
         return modelAndView;
     }
 }
